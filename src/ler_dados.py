24b37f0bda037cede6c299b352e7a9da21c48fdd
Guilherme Sampaio <pcoelhobr@gmail.com>
2026-08-18 16:08:05 -0300

    First Commit

---
+++ src/ler_dados.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+import pandas as pd
+
+base_dir = Path(__file__).resolve().parent
+path = base_dir.parent / "dados" / "bronze" / "gpu_database.csv"
+
+df = pd.read_csv(path)
+
+print(df.shape)
