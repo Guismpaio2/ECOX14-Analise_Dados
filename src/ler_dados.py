a7ca7803ab2d6e8fa96d98ce38d5e237deb5f58a
Guilherme Sampaio <pcoelhobr@gmail.com>
2026-08-20 17:36:00 -0300

    add(Dados) = implementamos a extração dos dados do kaggle e a sua organização no bronze/gpu

--- src/ler_dados.py
+++ src/ler_dados.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 
 base_dir = Path(__file__).resolve().parent
-path = base_dir.parent / "dados" / "bronze" / "gpu_database.csv"
+path = base_dir.parent / "dados" / "bronze" / "gpu" / "gpu_database.csv"
 
 df = pd.read_csv(path)
 
